e3a15e97c0fd470dbf2cc3a9995995d21a34b414
Ryan <ryan.whitemore@gmail.com>
2022-08-04 12:44:01 -0400

    Initial commit

---
+++ randomMessage.js
@@ -0,0 +1,27 @@
+let components = {setUp: ["How does a", "How does a" ],
+                subject: ["Baby", "Person", "Cow", "Alien", "Dinosaur", "Dog", "Stick", "Tree", "Plant"],
+                action: ["Work out?", "Make money?", "Get to work?", "Call their mom?", "Ask for directions?"],
+                punchline: ["They dont", "Poorly", "Very carefully", "Very well", "Milk", "They cry", "With great difficulty", "On tuesdays"] };
+
+function pickRandom(object) {
+    let message = []
+    for (const i in object) {
+        let index = Math.floor(Math.random() * object[i].length)
+        message.push(object[i][index])
+        
+    }
+    return message.join(' ')
+}
+function displayMessage() {
+   joke.innerHTML = pickRandom(components)
+}
+function changeText() {
+    button.innerHTML = "ANOTHER JOKE?"
+}
+
+const joke = document.getElementById("joke")
+const button = document.getElementById("button")
+
+
+button.addEventListener("click", displayMessage)
+button.addEventListener("click", changeText )
